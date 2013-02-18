baa69afb43e610e926e44c1c5e34cb768f2380a5
Rex Sheng <shengning@gmail.com>
2013-02-18 09:29:22 +0800

    Initial Commit

---
+++ FaceReco/FRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FRAppDelegate.h
+//  FaceReco
+//
+//  Created by Rex Sheng on 2/18/13.
+//  Copyright (c) 2013 lognllc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FaceReco/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FaceReco
+//
+//  Created by Rex Sheng on 2/18/13.
+//  Copyright (c) 2013 lognllc.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRAppDelegate class]));
+	}
+}
